857cc3a4fe01b87da6f4e80e90814dfa1f47d1ff
Andrew-Schwartz <andrewschwartz59@gmail.com>
2019-02-15 10:19:23 -0500

    flag image

--- src/logic/Tile.kt
+++ src/logic/Tile.kt
@@ -6,12 +6,17 @@ import javafx.scene.image.ImageView
 @Suppress("EqualsOrHashCode")
 data class Tile(val x: Int, val y: Int) {
     var value: Int = 0
-    val startImage = Image("images/CoveredTile.png")
+    private val startImage = Image("images/CoveredTile.png")
     val imageView = ImageView(startImage)
+    var isFlagged = false
+    var isRevealed = false
 
     fun reset() {
         value = 0
-        imageView.image = startImage
+        isFlagged = false
+        isRevealed = false
+        if (imageView.image != startImage)
+            imageView.image = startImage
     }
 
     fun setImageMaxSize(length: Double) {
@@ -19,13 +24,40 @@ data class Tile(val x: Int, val y: Int) {
         imageView.fitHeight = length
     }
 
-    fun updateImage() {
+    fun reveal() {
+        isRevealed = true
         imageView.image = when (value) {
-            -1 -> Image("images/Mine.jpg")
-            0 -> Image("images/Empty.png") //TODO get this image
+            -1 -> Image("images/Mine.png")
+            0 -> Image("images/Empty.png")
             else -> Image("images/$value.png")
         }
     }
+
+    fun flag() {
+        if (isRevealed) return
+        if (isFlagged) {
+            imageView.image = startImage
+        } else {
+            imageView.image = Image("images/Flag.png")
+        }
+        isFlagged = !isFlagged
+    }
+
+    val isBomb get() = value == -1
+
+    val adjacentTiles
+        get() = arrayOf(
+            x - 1 to y - 1,
+            x to y - 1,
+            x + 1 to y - 1,
+
+            x - 1 to y,
+            x + 1 to y,
+
+            x - 1 to y + 1,
+            x to y + 1,
+            x + 1 to y + 1
+        )
 
     override fun equals(other: Any?): Boolean {
         return when (other) {
@@ -33,4 +65,6 @@ data class Tile(val x: Int, val y: Int) {
             else -> false
         }
     }
+
+    override fun toString() = "Tile(x = $x, y = $y, value = $value)"
 }
